f5517bbd3de6caa6d7e9fb2e9339d218149fa4b9
iiacobac <iiacobac@b7f06f1d-8203-4af0-e9a6-22d325ffd685>
2010-05-02 17:16:18 +0000

    Initial import.

---
+++ IA/src/ar/uba/dc/ia2010/ej1/Utils.java
@@ -0,0 +1,12 @@
+package ar.uba.dc.ia2010.ej1;
+
+public class Utils {
+	
+	public static int zeroPosition(int[] state) {
+		for (int i = 0; i < state.length; i++) 
+			if (state[i]==0)
+				return i;
+		return -1;		
+	}
+	
+}
